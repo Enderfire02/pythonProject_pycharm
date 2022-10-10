3bf5111742aee32d48aa950a21351a102308af64
Cyrus <74969028+Enderfire02@users.noreply.github.com>
2022-10-10 12:15:08 +0200

    stup + cours

--- test.py
+++ test.py
@@ -1,3 +1,38 @@
 print("hello")
 print("camenbert")
+x = 5
+y = "John"
+print(x)
+print(y)
 
+variation_1 = 10
+variation_2 = 2
+
+variation_3 = True
+variation_4 = False
+
+print("variation_1 =", variation_1)
+print("variation_2 =", variation_2)
+variation_1, variation_2 = variation_2, variation_1
+print("variation_1 =", variation_1)
+print("variation_2 =", variation_2)
+
+print("variation_3 =", variation_3)
+print("variation_4 =", variation_4)
+variation_3, variation_4 = variation_4, variation_3
+print("variation_3 =", variation_3)
+print("variation_4 =", variation_4)
+
+chaine_1 = "Exemple"
+chaine_2 = 'Exemple2'
+print(chaine_2)
+print(chaine_1)
+
+chaine_3 = "j'aime le Python"
+print(chaine_3)
+
+'''
+somme = variation_1 + variation_2 + 3
+print(variation_1 + variation_2)
+print(somme)
+'''
