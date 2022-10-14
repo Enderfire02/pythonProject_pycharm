44851cc0b4f6ebc36ec4ab3fea29de153531b53b
Cyrus <74969028+Enderfire02@users.noreply.github.com>
2022-10-14 12:18:46 +0200

    Rendu cours 2

--- 14102022_cyrus_valette_EPSI/exercice 7.py
+++ 14102022_cyrus_valette_EPSI/exercice 7.py
@@ -1,12 +1,12 @@
 prenom = "Vincent"
 print(type(prenom))
-if type(prenom) is str:
+if type(prenom) == str:
     print("La variable est une chaine de caractère")
 else:
     print("pas le bon type")
 prenom = 0
-if type(prenom) is int:
-    print("bon type")
+if type(prenom) == int:
+    print("La variable est un entier")
 else:
     print("pas le bon type")
 
---
+++ 14102022_cyrus_valette_EPSI/exercice 8.py
@@ -0,0 +1,8 @@
+x = "salut les devs, salut "
+y = x.replace("salut", "bonsoir")
+print(y)
+
+#Replaçer seulement le premier salut
+x = "salut les devs, salut "
+y = x.replace("salut", "bonsoir", 1)
+print(y)
